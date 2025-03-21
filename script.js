0b734f1cabb945bd6c1992eaa3b298e1e18ed75a
Stjepan Svib <svib.stjepan@gmail.com>
2025-03-21 15:29:21 +0100

    Added randomComputerChoice and eventListeners

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded");
+//----Added DOM loader-----
+document.addEventListener("DOMContentLoaded",function(){});
 
-
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scisorss = document.querySelector("#scissors");
+//---- Added querySelectors-----
+const options = document.querySelector("#options");
+    const rock = document.querySelector("#rock");
+    const paper = document.querySelector("#paper");
+    const scissors = document.querySelector("#scissors");
 
 const roundWinnerDisplay = document.querySelector("#roundWinnerDisplay");
 const optionWinnerDisplay = document.querySelector("#optionWinnerDisplay");
@@ -17,3 +19,59 @@ const computerScore = document.querySelector("#computerScore");
 
 
 const restart = document.querySelector("#restart");
+
+let choices = ["rock", "paper", "scissors"];
+//-- function for computerChoice-----
+const randomComputerChoice = function(){
+    let choice;
+    choice = choices[Math.floor(Math.random()*3)]
+    return choice;
+};
+
+
+rock.addEventListener("click",function(){
+    switch(randomComputerChoice()){
+        case "rock":
+            computerDisplay.innerHTML = "&#128074";
+            break;
+        case "paper":
+            computerDisplay.innerHTML = "&#129306";
+            break;
+        case "scissors":
+            computerDisplay.innerHTML = "&#9996";
+            break;
+    }
+});
+
+paper.addEventListener("click",function(){
+    switch(randomComputerChoice()){
+        case "rock":
+            computerDisplay.innerHTML = "&#128074";
+            break;
+        case "paper":
+            computerDisplay.innerHTML = "&#129306";
+            break;
+        case "scissors":
+            computerDisplay.innerHTML = "&#9996";
+            break;
+    }
+});
+
+scissors.addEventListener("click",function(){
+    switch(randomComputerChoice()){
+        case "rock":
+            computerDisplay.innerHTML = "&#128074";
+            break;
+        case "paper":
+            computerDisplay.innerHTML = "&#129306";
+            break;
+        case "scissors":
+            computerDisplay.innerHTML = "&#9996";
+            break;
+    }
+});
+
+
+const playRound = function(){
+    
+}
