dcd4ab81bff3ac14f41d34fcd5e0539db0bd5d41
Stjepan Svib <svib.stjepan@gmail.com>
2025-03-21 15:07:03 +0100

    Added querySelectors

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded");
+
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scisorss = document.querySelector("#scissors");
+
+const roundWinnerDisplay = document.querySelector("#roundWinnerDisplay");
+const optionWinnerDisplay = document.querySelector("#optionWinnerDisplay");
+
+
+const playerDisplay = document.querySelector("#playerOption");
+const computerDisplay = document.querySelector("#computerOption");
+
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+
+
+const restart = document.querySelector("#restart");
